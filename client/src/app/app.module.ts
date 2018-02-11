5446afc668276cac2b854a1ee100400290774428
PavelDemyanenko <mail@paveldemyanenko.com>
2018-02-11 23:41:24 +0300

    Add user-list-component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { UserService } from './shared/user/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/shared/user/user.service.ts
+++ client/src/app/shared/user/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class UserService {
-
-  constructor() { }
-
+  constructor(private http: HttpClient) {
+  }
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/admin-users');
+  }
 }
