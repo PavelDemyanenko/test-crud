e896e75a27149f92dd63ef19eaa51357b3e1f8cd
PavelDemyanenko <mail@paveldemyanenko.com>
2018-02-11 23:47:41 +0300

    Import user list component in app-module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { UserService } from './shared/user/user.service';
+import { UserListComponent } from './user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
